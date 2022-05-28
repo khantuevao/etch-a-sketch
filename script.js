8627981ad6c2a1e669ad63cb2f0091a34f7cde61
khantuevao <khantuevao@gmail.com>
2022-05-28 15:35:32 +0200

    Add hover effect to change color for created divs

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const mainContainer = document.querySelector('#mainContainer');
 
+const createRow = function() {
+    row = document.createElement('div');
+    row.classList.add('row');
+    mainContainer.appendChild(row);
+}
+
+const createColumn = function() {
+    column = document.createElement('div');
+    column.setAttribute('id', 'columnid')
+    column.classList.add('column');         
+    row.appendChild(column);
+    return
+}
 
 function createGrid() {
     for (i = 0; i < 16; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        mainContainer.appendChild(row);
+        createRow();
         for (j = 0; j < 16; j++) {
-            let column = document.createElement('div');
-            column.classList.add('column');         
-            row.appendChild(column);
+            createColumn();
         }
     }
-    return
+
+    let columns = document.querySelectorAll('.column');
+    columns.forEach(column => column.addEventListener('mouseover', function(event) {
+        event.target.classList.add('hover');
+        setTimeout(function() {
+            event.target.classList.remove('hover');
+        }, 500);
+    }, false));
 }
 
 const btnStart = document.querySelector('#btnStart');
@@ -20,4 +36,5 @@ btnStart.addEventListener('click', () => {
     createGrid();
     btnStart.style.display = 'none';
 });
+
 
