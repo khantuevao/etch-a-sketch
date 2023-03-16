8018d225e4f30b2a83fc171d263d3224c7017f93
khantuevao <khantuevao@gmail.com>
2023-03-16 18:32:54 +0600

    Add button to generate a new grid with a selected size

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 const mainContainer = document.getElementById('main-container')
 
-//create 16 containers & put inside main-container
-for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
-    square.addEventListener('mouseover', () => {
-        square.style.cssText = 'background-color: lightpink'
-    })
-    mainContainer.appendChild(square)
+const newGrid = document.createElement('button')
+newGrid.textContent = 'Create new grid'
+newGrid.addEventListener('click', () => {
+    let gridSize = prompt('Enter a number', 0)
+    clearGrid()
+    createNewGrid(gridSize)
+})
+document.body.appendChild(newGrid)
+
+function clearGrid() {
+    mainContainer.innerHTML = ''
 }
+
+function createNewGrid(num) {
+    //set limitations
+    if (num > 100) {
+        alert('Cannot be over 100')
+        return
+    }
+    if (isNaN(num)) {
+        alert('Should be a number')
+        return
+    }
+    //create fixed number of rows
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        //append to each row a fixed number of squares
+        for (let j = 0; j < num; j++) {
+            const square = document.createElement('div')
+            square.classList.add('square')
+            //change square class on hover
+            square.addEventListener('mouseover', () => {
+                square.classList.add('selected')
+            })
+            row.appendChild(square)
+        }
+        mainContainer.appendChild(row)
+    }
+}
+
+createNewGrid(16)
